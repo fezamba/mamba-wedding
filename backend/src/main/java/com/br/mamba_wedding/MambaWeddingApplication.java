0fe0372a30f0ec3c624e5bcbf49de2b9a3c3ed37
fezamba <fepetitt@gmail.com>
2026-03-21 14:40:22 -0300

    .

--- backend/src/main/java/com/br/mamba_wedding/MambaWeddingApplication.java
+++ backend/src/main/java/com/br/mamba_wedding/MambaWeddingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MambaWeddingApplication {
 
+	// TODO: Set variables, functions, exceptions and getters/setters names to english standard
 	public static void main(String[] args) {
 		SpringApplication.run(MambaWeddingApplication.class, args);
 	}
