b773cd5ff82216439dd85e0946b4c620b689eb42
fezamba <fepetitt@gmail.com>
2026-03-31 15:33:39 -0300

    create frontend structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
+import { AuthProvider } from './context/AuthContext'
+import AppRoutes from './routes/AppRoutes'
+
 function App() {
   return (
-    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-slate-800">
-        Megumi Fushiguro is the Goat.
-      </h1>
-    </div>
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
   )
 }
 
---
+++ frontend/src/types/auth.ts
@@ -0,0 +1,7 @@
+export type LoginRequest = {
+  codigoConvite: string
+}
+
+export type LoginResponse = {
+  token: string
+}
